fe56a6bbcdbb438e0af8e8a84ce68e4aa9e60dce
Rhys <rbrooker105@gmail.com>
2017-07-18 19:58:17 +1000

    refactor bullet click listener

--- scripts/image-slider.js
+++ scripts/image-slider.js
@@ -1,8 +1,3 @@
-const bulletZero = document.getElementById('bullet-0');
-const bulletOne = document.getElementById('bullet-1');
-const bulletTwo = document.getElementById('bullet-2');
-const bulletThree = document.getElementById('bullet-3');
-const bulletFour = document.getElementById('bullet-4');
 const leftArrow = document.getElementById('left-arrow');
 const rightArrow = document.getElementById('right-arrow');
 const imageStrip = document.getElementById('image-strip');
@@ -11,9 +6,9 @@ const imageClass = {
   1: 'image-1',
   2: 'image-2',
   3: 'image-3',
-  4: 'image-4'
+  4: 'image-4',
 };
-const bullets = [bulletZero, bulletOne, bulletTwo, bulletThree, bulletFour];
+const bullets = document.getElementsByClassName('bullets');
 let imagePosition = 0;
 
 
@@ -24,6 +19,7 @@ const toggleImageClass = function toggleImageClass(position) {
 
 
 rightArrow.addEventListener("click", function() {
+  console.log('click');
   toggleImageClass(imagePosition);
   imagePosition += 1;
   toggleImageClass(imagePosition);
@@ -42,6 +38,6 @@ const moveToBulletClick = function moveToBulletClick() {
   toggleImageClass(imagePosition);
 }
 
-bullets.forEach(function(element) {
+Array.from(bullets).forEach(function(element) {
   element.addEventListener('click', moveToBulletClick);
 });
